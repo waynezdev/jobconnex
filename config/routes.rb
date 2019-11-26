7b285629259514261367609e6eb52fd89ac6bfbb
waynezdev <cas021919@coderacademy.edu.au>
2019-11-26 20:49:04 +1100

    updated user control and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,36 @@
 Rails.application.routes.draw do
   resources :jobs
-  devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users, controllers: { sessions: 'users/sessions' }
 
   root to: "jobs#index"
 end
+
+=begin
+jobs GET    /jobs(.:format)                                                                          jobs#index
+POST   /jobs(.:format)                                                                          jobs#create
+GET    /jobs/new(.:format)                                                                      jobs#new
+GET    /jobs/:id/edit(.:format)                                                                 jobs#edit
+GET    /jobs/:id(.:format)                                                                      jobs#show
+PATCH  /jobs/:id(.:format)                                                                      jobs#update
+PUT    /jobs/:id(.:format)                                                                      jobs#update
+DELETE /jobs/:id(.:format)                                                                      jobs#destroy
+
+
+new_user_session GET    /users/sign_in(.:format)                                                 users/sessions#new
+user_session POST   /users/sign_in(.:format)                                                     users/sessions#create
+destroy_user_session DELETE /users/sign_out(.:format)                                            users/sessions#destroy
+new_user_password GET    /users/password/new(.:format)                                           devise/passwords#new
+edit_user_password GET    /users/password/edit(.:format)                                         devise/passwords#edit
+user_password PATCH  /users/password(.:format)                                                   devise/passwords#update
+PUT    /users/password(.:format)                                                                devise/passwords#update
+POST   /users/password(.:format)                                                                devise/passwords#create
+cancel_user_registration GET    /users/cancel(.:format)                                         devise/registrations#cancel
+new_user_registration GET    /users/sign_up(.:format)                                           devise/registrations#new
+edit_user_registration GET    /users/edit(.:format)                                             devise/registrations#edit
+user_registration PATCH  /users(.:format)                                                       devise/registrations#update
+PUT    /users(.:format)                                                                         devise/registrations#update
+DELETE /users(.:format)                                                                         devise/registrations#destroy
+POST   /users(.:format)                                                                         devise/registrations#create
